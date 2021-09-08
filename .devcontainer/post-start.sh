15350b43dcb6bf35815851f26a04840d65648d1c
Tim Park <timfpark@gmail.com>
2021-09-07 19:13:56 -0700

    Remove deploy directory

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -4,7 +4,7 @@ echo "post-start start" >> ~/status
 
 # this runs in background each time the container starts
 
-k3d cluster create --registry-use k3d-registry.localhost:5500 --config deploy/k3d.yaml --k3s-server-arg "--no-deploy=traefik" --k3s-server-arg "--no-deploy=servicelb"
+k3d cluster create --registry-use k3d-registry.localhost:5500 --config .devcontainer/k3d.yaml --k3s-server-arg "--no-deploy=traefik" --k3s-server-arg "--no-deploy=servicelb"
 
 # wait for cluster to be ready
 kubectl wait node --for condition=ready --all --timeout=60s
