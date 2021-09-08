5dfabf17474e7e093f41965e0656b82f2191228c
Tim Park <timfpark@gmail.com>
2021-09-08 15:14:16 +0000

    Remove bootstrap commands

--- .devcontainer/bootstrap.sh
+++
@@ -1,1 +0,0 @@
-GITHUB_TOKEN=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 flux bootstrap github --owner=timfpark --components-extra=image-reflector-controller,image-automation-controller --repository=workload-cluster-gitops --branch=main --path=tim-dev --personal --network-policy=false
