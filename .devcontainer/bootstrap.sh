57e57580634813bdb9641865eab4419207be22c7
Tim Park <timfpark@gmail.com>
2021-09-07 20:50:04 -0700

    Add bootstrap command

---
+++ .devcontainer/bootstrap.sh
@@ -0,0 +1,1 @@
+GITHUB_TOKEN=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 flux bootstrap github --owner=timfpark --components-extra=image-reflector-controller,image-automation-controller --repository=workload-cluster-gitops --branch=main --path=tim-dev --personal --network-policy=false
