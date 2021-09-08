a0e5ca682240cb766712fa45cc41c9d90953f1bd
Tim Park <timfpark@gmail.com>
2021-09-07 19:20:26 -0700

    Add Flux install

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -5,11 +5,14 @@ echo "post-create start" >> ~/status
 # this runs in background after UI is available
 
 # (optional) upgrade packages
-#sudo apt-get update
-#sudo apt-get upgrade -y
-#sudo apt-get autoremove -y
-#sudo apt-get clean -y
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get autoremove -y
+sudo apt-get clean -y
 
 # add your commands here
+
+curl -s https://fluxcd.io/install.sh | sudo bash
+chmod 777 /usr/local/bin/flux
 
 echo "post-create complete" >> ~/status
