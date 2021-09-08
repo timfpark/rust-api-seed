6fd11be2e454bb71b1a627d4395c38ed3c07b0e1
Tim Park <timfpark@gmail.com>
2021-09-08 19:44:37 +0000

    Fix bootstrap-flux

--- tools/bootstrap-flux.sh
+++ tools/bootstrap-flux.sh
@@ -3,23 +3,28 @@
 # Clone workload cluster repo
 
 cd /workspaces
-git clone https://$GITHUB_USER:$GITHUB_TOKEN@github.com/timfpark/workload-gitops-repo
 
-export GITHUB_USER=`$CODESPACE_NAME | cut -d'-' -f 1`
+echo $GITHUB_USER
+echo $GITHUB_TOKEN
+
+git clone https://$GITHUB_USER:$GITHUB_TOKEN@github.com/timfpark/workload-cluster-gitops
+cd workload-cluster-gitops
+
 export CODE_BRANCH=`git rev-parse --symbolic-full-name --abbrev-ref HEAD`
 export GITOPS_CODESPACE_APP_PATH=codespaces/$RepositoryName
 export GITOPS_USER_CODESPACE_PATH=$GITOPS_CODESPACE_APP_PATH/$GITHUB_USER
 export GITOPS_BRANCH_CODESPACE_PATH=$GITOPS_USER_CODESPACE_PATH/$CODE_BRANCH
 
-mkdir -p $USER_CODESPACE_PATH
+mkdir -p $GITOPS_USER_CODESPACE_PATH
 
 # Template out codespace workload if it doesn't exist
-if [ ! -d "$BRANCH_CODESPACE_PATH" ]
+if [ -d "$GITOPS_BRANCH_CODESPACE_PATH" ]
 then
-    cp -r $CODESPACE_APP_PATH/template $BRANCH_CODESPACE_PATH
+    echo "Tempalting out path"
+    cp -r $GITOPS_CODESPACE_APP_PATH/template $GITOPS_BRANCH_CODESPACE_PATH
 
     git add -A $GITOPS_BRANCH_CODESPACE_PATH
-    git commit -m 'Template out $CODE_BRANCH branch for app $RepositoryName for codespace'
+    git commit -m "Template out $CODE_BRANCH branch for app $RepositoryName for codespace"
     git push origin main
 fi
 
